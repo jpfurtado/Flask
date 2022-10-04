10713089333f63d7fff1dce6b102ba1740d61d56
Joao Furtado <joao.furtado@prof.una.br>
2022-10-03 21:36:17 -0300

    Aula 03/10/2022

--- HelloWorld/hello.py
+++ HelloWorld/hello.py
@@ -1,18 +1,14 @@
 # Importação da microframework Flask  
 from flask import Flask
-
 app = Flask(__name__)
-
 # Rota é o caminho relativo do seu site a URL que será acessada
 @app.route("/")
 # Função é o que será executado 
 def hello():
-    return "Hello World!!!"
-
+    return "<H1>Hello World!!!</H1>"
 @app.route("/usuarios/<usuario>")
 def usuarios(usuario):
-    return ("Hello, " + usuario )
-
+    return ("<H2>Hello, " + usuario + "</H2>" )
 # Coloca o site no ar
 if __name__ == '__main__':
     app.run(debug=True)
